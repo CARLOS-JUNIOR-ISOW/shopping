4418ec015219c2d775904d2afceb3f0d893cb817
Carlos Junior <carlosjunior@isow.com.br>
2022-07-04 14:42:12 -0300

    Primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/themes/light.theme.dart';
+import 'pages/tabs.page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme(),
+      home: DefaultTabController(
+        length: 3,
+        child: TabsPage(),
+      ),
+    );
+  }
+}
